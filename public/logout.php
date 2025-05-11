97617e5b2dd7ab5ef0f8854301fe794ca0b62ba2
HamitEldem <meydanlar-guveler0l@icloud.com>
2025-05-11 15:06:43 +0300

    Finalized logout.php

--- public/logout.php
+++ public/logout.php
@@ -1,7 +1,24 @@
 <?php
-  require "../app/templates/header.php";
-  require "../app/templates/navbar.php";
 
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+
+
+
+require_once "../app/includes/db.php" ;
+
+
+ // delete remember me part
+ setTokenByEmail($_SESSION["user"]["email"], null) ;
+ setcookie("access_token", "", 1) ; 
+
+ // delete session file
+ session_destroy() ;
+ // delete session cookie
+ setcookie("PHPSESSID", "", 1 , "/") ; // delete memory cookie 
+
+ // redirect to landing page.
+ header("Location: index.php") ;
 ?>
 
-<?php require "../app/templates/footer.php";?>
