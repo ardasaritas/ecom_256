ecebdbfdbd2552ee883db87bf9227fbfea981c19
HamitEldem <meydanlar-guveler0l@icloud.com>
2025-05-11 15:08:31 +0300

    Implemented backend of email verify, frontend should be implemented

--- public/verify_email.php
+++ public/verify_email.php
@@ -1,14 +1,52 @@
 <?php
-# Verify Email Logic, proposal to change to email.php under includes and than require it where needed
+
+require_once "../app/includes/db.php" ;
+
+//check if user is coming from register.php - otherwise there won't be any cookie -
+if($_COOKIE["email"] != ""){
+    $user = getUserByEmail($_COOKIE["email"]);
+
+
+    if($user["is_verified"] == 0){
+        if(!empty($_POST)){
+            extract($_POST);
+
+            if(isset($otp)){
+                if($otp == $user["verification_code"]){
+
+                    setUserVerified($_COOKIE["email"]);
+
+                    // destroy cookie
+                    unset($_COOKIE["email"]);
+                    setcookie("email", "", time() - 3600, "/"); 
+
+                    //user is verified, route user to login.php
+                    header("Location: login.php");
+                } else{
+                    $out = "Wrong code, enter again" ;
+                }
+            }
+    }
+    }
+} else{
+    header("Location: index.php");
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Email Verify</title>
+
 </head>
 <body>
-    VERIFY
+    <form action="" method = "post" >
+        <p>Enter the 6-Digit Code</p>
+        <input type="text" inputmode="numeric" maxlength="6" placeholder="Enter OTP" name = "otp">
+        <button type="submit">Verify Email</button>
+    </form>
 </body>
 </html>
