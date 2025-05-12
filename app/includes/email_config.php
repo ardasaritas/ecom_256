f892d201b5375d5d86e87e1759d690f7a40b18d3
HamitEldem <meydanlar-guveler0l@icloud.com>
2025-05-12 14:37:46 +0300

    added email_config.php

---
+++ app/includes/email_config.php
@@ -0,0 +1,9 @@
+<?php
+
+// config.php
+define('EMAIL', 'youremail@gmail.com'); //enter your email here
+define('PASSWORD', 'PASSWORD HERE');  // the 16-char password from Google
+define('FULLNAME', 'EXPIRY SAVER');
+
+
+?>
