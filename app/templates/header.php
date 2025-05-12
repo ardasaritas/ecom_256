b2adb81a057150f96502cd925a7d8d90a0ceaf17
ardasaritas <arda.saritas@ug.bilkent.edu.tr>
2025-05-12 15:52:23 +0300

    header.php minor fix

--- app/templates/header.php
+++ app/templates/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="/public/assets/style.css">
+  <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
   <div class="d-flex flex-column min-vh-100"> <!-- wrapper starts (for sticky footer)-->
