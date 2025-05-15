178613628eaab8b90f318dd8e831f18f742a16bd
ardasaritas <143201588+ardasaritas@users.noreply.github.com>
2025-05-16 01:23:34 +0300

    Stop tracking email_config.php

--- app/includes/email_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// config.php
-define('EMAIL', 'EMAIL'); //enter your email here
-define('PASSWORD', 'PASSWORD');  // the 16-char password from Google
-define('FULLNAME', 'EXPIRY SAVER');
-
-
-?>
